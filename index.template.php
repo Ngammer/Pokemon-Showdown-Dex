8e771d7c64179318876b33efd634bfd0215215b8
Ngammer <cnicolas810@gmail.com>
2025-06-02 13:21:39 -0300

    upd

--- index.template.php
+++ index.template.php
@@ -6,11 +6,11 @@
 	<title>Pok&eacute;dex - Pok&eacute;mon Showdown</title>
 
 	<meta http-equiv="X-UA-Compatible" content="IE=Edge,chrome=IE8" />
-	<link rel="stylesheet" href="//play.pokemonshowdown.com/style/font-awesome.css" />
+	<link rel="stylesheet" href="//190.190.130.232:3000/style/font-awesome.css" />
 	<link rel="stylesheet" href="/theme/panels.css?" />
 	<link rel="stylesheet" href="/theme/main.css?" />
 	<meta name="viewport" content="width=device-width,initial-scale=1">
-	<link rel="stylesheet" href="//play.pokemonshowdown.com/style/utilichart.css?" />
+	<link rel="stylesheet" href="//190.190.130.232:3000/style/utilichart.css?" />
 	<link rel="stylesheet" href="/theme/pokedex.css?" />
 
 	<!-- Workarounds for IE bugs to display trees correctly. -->
@@ -29,7 +29,7 @@
 				<li><a class="button nav-last" href="//pokemonshowdown.com/forums/">Forum</a></li>
 			</ul>
 			<ul class="nav nav-play">
-				<li><a class="button greenbutton nav-first nav-last" href="http://play.pokemonshowdown.com/">Play</a></li>
+				<li><a class="button greenbutton nav-first nav-last" href="http://190.190.130.232:3000/">Play</a></li>
 			</ul>
 			<div style="clear:both"></div>
 		</div>
@@ -67,20 +67,20 @@
 	<script src="/js/lib/backbone-min.js"></script>
 	<script src="/js/panels.js?"></script>
 
-	<script src="//play.pokemonshowdown.com/config/config.js?"></script>
-	<script src="//play.pokemonshowdown.com/js/battledata.js?"></script>
-	<script src="//play.pokemonshowdown.com/data/search-index.js?"></script>
-	<script src="//play.pokemonshowdown.com/data/pokedex.js?"></script>
-	<script src="//play.pokemonshowdown.com/data/teambuilder-tables.js?"></script>
-	<script src="//play.pokemonshowdown.com/data/learnsets.js?"></script>
-	<script src="//play.pokemonshowdown.com/data/moves.js?"></script>
-	<script src="//play.pokemonshowdown.com/data/abilities.js?"></script>
-	<script src="//play.pokemonshowdown.com/data/items.js?"></script>
-	<script src="//play.pokemonshowdown.com/data/formats-data.js?"></script>
-	<script src="//play.pokemonshowdown.com/data/typechart.js?"></script>
-	<script src="//play.pokemonshowdown.com/data/aliases.js?"></script>
-	<script src="//play.pokemonshowdown.com/js/battle-dex-search.js?"></script>
-	<script src="//play.pokemonshowdown.com/js/search.js?"></script>
+	<script src="//190.190.130.232:3000/config/config.js?"></script>
+	<script src="//190.190.130.232:3000/js/battledata.js?"></script>
+	<script src="//190.190.130.232:3000/data/search-index.js?"></script>
+	<script src="//190.190.130.232:3000/data/pokedex.js?"></script>
+	<script src="//190.190.130.232:3000/data/teambuilder-tables.js?"></script>
+	<script src="//190.190.130.232:3000/data/learnsets.js?"></script>
+	<script src="//190.190.130.232:3000/data/moves.js?"></script>
+	<script src="//190.190.130.232:3000/data/abilities.js?"></script>
+	<script src="//190.190.130.232:3000/data/items.js?"></script>
+	<script src="//190.190.130.232:3000/data/formats-data.js?"></script>
+	<script src="//190.190.130.232:3000/data/typechart.js?"></script>
+	<script src="//190.190.130.232:3000/data/aliases.js?"></script>
+	<script src="//190.190.130.232:3000/js/battle-dex-search.js?"></script>
+	<script src="//190.190.130.232:3000/js/search.js?"></script>
 	<script src="/js/pokedex.js?"></script>
 	<script src="/js/pokedex-pokemon.js?"></script>
 	<script src="/js/pokedex-moves.js?"></script>
